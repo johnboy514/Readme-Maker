47e544bb98cbaf42af6678ea9c880e4d85017618
Swag <john.ifert.miller@gmail.com>
2022-11-11 16:02:17 -0500

    HtmlContent funtions

--- index.js
+++ index.js
@@ -30,7 +30,29 @@ inquirer
         message: "What is your Github?"
     },
 ])
-
+.then((answer) => {
+    let htmlContent = 
+    '<html>\n\
+            <head>\n\
+                <meta charset="UTF-8"><link rel="stylesheet" href="style.css">\n\
+                <title>Portfolio</title>\n\
+            </head>\n\
+            <body>\n\
+                <header>\n\
+                    <h1 class="title">Portfolio</h1>\n\
+                </header>\n\
+                <h2 id="name">' + answer.name + '</h2>\n\
+                <p id="bio" >' + answer.bio + '</p>\n\
+                <h2 class="title">Contact Info</h2>\n\
+                <ul>\n\
+                    <li id="location"><strong>Location:</strong>' + answer.location + '</li>\n\
+                    <li id="linkedIn"><strong>LinkedIn:</strong>' + answer.linkedIn + '</li>\n\
+                    <li id="github"><strong>Github:</strong>' + answer.gitHub + '</li>\n\
+                </ul>\n\
+            </body>\n\
+    </html>';
+    fs.appendFile('index.html', htmlContent, (error) => error ? console.error(error) : console.log("Saved!"));
+});
 // TODO: Create an array of questions for user input
 const questions = [];
 

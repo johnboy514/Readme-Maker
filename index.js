72c964df147fa8c3afa8a263f6cf40b8fce8e6d7
Swag <john.ifert.miller@gmail.com>
2022-11-11 16:00:29 -0500

    built questions

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require(`fs`);
+const inquirer = require(`inquirer`);
+
+inquirer
+.createPromptModule([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "bio",
+        message: "What is your bio?"
+    },
+    {
+        type: "input",
+        name: "location",
+        message: "What is your location?"
+    },
+    {
+        type: "input",
+        name: "linkedIn",
+        message: "What is your linkedIn?"
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your Github?"
+    },
+])
 
 // TODO: Create an array of questions for user input
 const questions = [];

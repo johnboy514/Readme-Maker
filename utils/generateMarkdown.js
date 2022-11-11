2bc9d53637694d0801644e20a9ac91448fa24e06
Swag <john.ifert.miller@gmail.com>
2022-11-11 16:36:23 -0500

    maybe funtions?

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,58 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return`
 
+# ${answers.title}
+
+## Description
+
+${answers.description}
+
+## Table of COntents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installations
+
+${answers.installations}
+
+## Usage
+
+${answers.usage}
+
+## Credits
+
+${answers.credits}
+
+## License
+
+${answers.license}
+
+## Features
+
+${answers.features}
+
+## How to Contribute
+
+${answers.contribute}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+My Github: [${answers.github}](https://github.com/${answers.gitHub})<br>
+Email me at: ${answers.email} for any questions
 `;
 }
 
